36ccec7819552dfd00b338508b94059549e84b63
Nahom Assefa <ndagfc@mail.missouri.edu>
2018-07-24 13:51:41 -0500

    Converted the string columns into ints and stored them into new list

--- Program.cs
+++ Program.cs
@@ -8,12 +8,13 @@ namespace CrimeAnalyzer
 
 
 
-    class CrimeStats { 
-    
-        
-        private List <int> Rape = new List<int>();
-    
-    
+    class CrimeStats
+    {
+
+
+        private List<int> Rape = new List<int>();
+
+
     }
     class Program
     {
@@ -32,31 +33,69 @@ namespace CrimeAnalyzer
                 /// read first line 
                 line = sr.ReadLine();
 
-              
-                string[] titles = line.Split(',');
+
+                Console.WriteLine(line);
+
+
+                string[] titles = line.Split(',');  // Years Population , etc 
+
                 line = sr.ReadLine();
 
 
-                List<string> yearList = new List<string>();
-                List<string> Population = new List<string>(); 
-                 
-                while (line != null) {
-                    string [] columns = line.Split(',');  // columns[0] == year columns[1] == population columns[2] == Violent Crime
+                List<int> Year = new List<int>();
+                List<int> Population = new List<int>();
+                List<int> Violent_Crime = new List<int>();
+                List<int> Murder = new List<int>();
+                List<int> Rape = new List<int>();
+                List<int> Robbery = new List<int>();
+                List<int> Aggravated_Assault = new List<int>();
+                List<int> Property_Crime = new List<int>();
+                List<int> Burglary = new List<int>();
+                List<int> Theft = new List<int>();
+                List<int> Motor_Vehicle_Theft = new List<int>();
+              
 
-                    yearList.Add(columns[0]);
-                    Population.Add(columns[1]); 
+
+                while (line != null)
+                {
+                    string[] columns = line.Split(',');  // columns[0] == year columns[1] == population columns[2] == Violent Crime
+
+                    Year.Add(Convert.ToInt32(columns[0]));
+                    Population.Add(Convert.ToInt32(columns[1]));
+                    Violent_Crime.Add(Convert.ToInt32(columns[2]));
+                    Murder.Add(Convert.ToInt32(columns[3]));
+                    Rape.Add(Convert.ToInt32(columns[4]));
+                    Robbery.Add(Convert.ToInt32(columns[5]));
+                    Aggravated_Assault.Add(Convert.ToInt32(columns[6]));
+                    Property_Crime.Add(Convert.ToInt32(columns[7]));
+                    Burglary.Add(Convert.ToInt32(columns[8]));
+                    Theft.Add(Convert.ToInt32(columns[9]));
+                    Motor_Vehicle_Theft.Add(Convert.ToInt32(columns[10]));
+
+
                     line = sr.ReadLine();
 
-                    
+
                 }
 
 
-                foreach(string t in yearList) { 
-                     Console.WriteLine(t);
+                //foreach(int t in yearList) { // loop through each year 
+                //     Console.WriteLine(t);
+                //}
+
+
+                IEnumerable<int> q1 = from y in Year
+                                      where y > 1997
+                                      select y;
+
+                Console.WriteLine("Years greater than 1997: ");
+
+                foreach (int t in q1)
+                { // loop through each IEnumerable result ("select year where its greater than 1997") 
+                    Console.WriteLine(t);
                 }
 
-
-
+                sr.Close();
 
             }
 
