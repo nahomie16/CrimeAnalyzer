6f47a080e6ae90d92e48cd21789ba25992858801
Nahom Assefa <ndagfc@mail.missouri.edu>
2018-07-24 02:04:45 -0500

    Creatred a StreamReader and a try and catch block

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CrimeAnalyzer
+{
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            try
+            {
+
+                Console.WriteLine("Enter the File name:");
+
+                string filename = Console.ReadLine();
+                StreamReader sr = new StreamReader(filename);
+
+                line = sr.ReadLine();
+                Console.WriteLine(line);
+
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception:" + e.Message);
+            }
+
+            finally
+            {
+
+                Console.WriteLine("final block");
+            }
+
+        }
+    }
+}
