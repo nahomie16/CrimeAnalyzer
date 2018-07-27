0d51df09503c228d8abd51183afd5b6ad453fe03
Nahom Assefa <ndagfc@mail.missouri.edu>
2018-07-26 20:15:28 -0500

    Answered all the questions

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.IO;
 namespace CrimeAnalyzer
 {
 
- 
+
     class CrimeStats
     {
 
@@ -102,42 +102,55 @@ namespace CrimeAnalyzer
 
 
                 }
-       
+
                 // What is the range of years included in the data?
                 IEnumerable<int> q1 = from crimeStats in crimeStatsList
-                                      where crimeStats.year <= 2013 && crimeStats.year >= 1994
-                                      select crimeStats.year; 
+                                      where crimeStats.year >= 1994 && crimeStats.year < 2014
+                                      select crimeStats.year;
 
-                Console.WriteLine($"Period {q1.Min()} - {q1.Max()}  ({q1.Max() - q1.Min()} years)"); 
+                Console.WriteLine($"Period {q1.Min()} - {q1.Max()}  ({q1.Max() - q1.Min()} years)");
 
 
                 // How many years of data are included?
                 IEnumerable<int> q2 = from crimeStats in crimeStatsList
-                                      where crimeStats.year > 1
                                       select crimeStats.year;
 
-                Console.WriteLine(q2.Count()); 
+                Console.WriteLine($"Year count {q2.Count()}");
 
 
                 // What years is the number of murders per year less than 15000?
-                IEnumerable<int> q3 =   from crimeStats in crimeStatsList 
-                                         where crimeStats.murder < 15000
-                                        select crimeStats.year;
-                foreach(int y in q3 ){
+                IEnumerable<int> q3 = from crimeStats in crimeStatsList
+                                      where crimeStats.murder < 15000
+                                      select crimeStats.year;
+                
+                Console.Write($"Years murders per year< 15000: ");
 
-                    Console.WriteLine(y); 
+                foreach (int x in q3)
+                {
+                    Console.Write($"{x}, ");
                 }
-                Console.WriteLine(" -------------------");
+                Console.WriteLine("\n");
 
                 // What are the years and associated robberies per year for years where the number of robberies is greater than 500000 ?
                 IEnumerable<int> q4 = from crimeStats in crimeStatsList
                                       where crimeStats.robbery > 500000
                                       select crimeStats.year;
+                // Console.WriteLine("Robberies per year > 500000 "); 
 
-                foreach(int y in q4 ){
-                    
-                    Console.WriteLine(y);
+                // What is the violent crime per capita rate for 2010? Per capita rate is the number of violent crimes in a year divided by the size of the population that year.
+
+                IEnumerable<double> q5 = from crimeStats in crimeStatsList
+                                         where crimeStats.year == 2010
+                                         select crimeStats.violentCrime / (double)crimeStats.population;
+
+
+                Console.Write("Violent crime per capita rate(2010): ");  
+                foreach (double x in q5)
+                {
+                    Console.Write(x);
+
                 }
+                Console.WriteLine("\n");
 
 
                 // What is the average number of murders per year across all years? 
@@ -145,16 +158,61 @@ namespace CrimeAnalyzer
                                       select crimeStats.murder;
 
 
-                double avg_murder = q6.Average(); 
-                Console.WriteLine($"What is the average number of murders per year across all years ? {avg_murder}");
+                double avg_murder = q6.Average();
+                Console.WriteLine($"Average murder per year (all years):  {avg_murder}");
 
                 // What is the average number of murders per year for 1994 to 1997?
-
                 IEnumerable<int> q7 = from crimeStats in crimeStatsList
-                                      where crimeStats.year >= 1994 && crimeStats.year <= 1997 
+                                      where crimeStats.year >= 1994 && crimeStats.year <= 1997
                                       select crimeStats.murder;
 
+                double avg_murder2 = q7.Average();
+                Console.WriteLine($"Average murder per year (1994-1997) {avg_murder2}");
 
+
+                // What is the average number of murders per year for 2010 to 2013?
+                IEnumerable<int> q8 = from crimeStats in crimeStatsList
+                                      where crimeStats.year >= 2010 && crimeStats.year <= 2013
+                                      select crimeStats.murder;
+
+                double avg_murder3 = q8.Average();
+                Console.WriteLine($"Average murder per year (2010-2013) {avg_murder3}");
+
+                // What is the minimum number of thefts per year for 1999 to 2004?
+                IEnumerable<int> q9 = from crimeStats in crimeStatsList
+                                      where crimeStats.year >= 1999 && crimeStats.year <= 2004
+                                      select crimeStats.theft;
+
+
+                Console.WriteLine($"Minimum thefts per year (1999-2004): {q9.Min()}");
+
+
+                // QUESTION 10  What is the maximum number of thefts per year for 1999 to 2004 ?  
+                Console.WriteLine($"Maximum thefts per year (1999-2004): {q9.Max()}");
+
+                // What year had the highest number of motor vehicle thefts?
+
+
+                IEnumerable<int> q11 = from crimeStats in crimeStatsList
+                                       where crimeStats.motorVehicleTheft > 1
+                                       select crimeStats.motorVehicleTheft;
+
+
+
+
+                int highest_murder = q11.Max();
+
+                // get year 
+                IEnumerable<int> q11_1 = from crimeStats in crimeStatsList
+                                         where crimeStats.motorVehicleTheft == highest_murder
+                                         select crimeStats.year;
+
+                foreach (int x in q11_1)
+                {
+
+                    Console.WriteLine($"Year of highest number of motor vehicle thefts {x}");
+                    break;
+                }
 
                 sr.Close();
 
